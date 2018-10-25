ed7390bb6b6642c89c4bdc6849bc5c7b644e5a7f
Moria <moria.am@gmail.com>
2018-10-25 13:53:25 +0200

    update header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import logo from './code-pub-digital.png'
+import logo from './code-pub-klarna.png'
 
 class Header extends Component {
   render() {
     return (
-      <header style={{ backgroundColor: '#222', padding: 20, ...this.props.style }}>
-        <img src={logo} style={{ height: 150 }} alt="logo" />
+      <header style={{ backgroundColor: '#222', ...this.props.style }}>
+        <img src={logo} style={{ width: '100%' }} alt="logo" />
       </header>
     )
   }
